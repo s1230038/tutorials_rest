19758b56d4033e24b476a5344e1199bada7e096a
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-06-07 22:03:06 +0900

    [update] Delete

--- payroll/src/main/java/com/example/payroll/EmployeeController.java
+++ payroll/src/main/java/com/example/payroll/EmployeeController.java
@@ -84,7 +84,8 @@ class EmployeeController {
   }
 
   @DeleteMapping("/employees/{id}")
-  void deleteEmployee(@PathVariable Long id) {
+  ResponseEntity<?> deleteEmployee(@PathVariable Long id) {
     repository.deleteById(id);
+    return ResponseEntity.noContent().build();
   }
 }
