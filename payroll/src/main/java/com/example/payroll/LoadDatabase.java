23ed6ea67ecad87fe625344f0eb7d5b4e9634242
Haru on Acer PC <s1230038uaizuacjp@gmail.com>
2024-06-10 17:03:45 +0900

    [add] order fulfillment

--- payroll/src/main/java/com/example/payroll/LoadDatabase.java
+++ payroll/src/main/java/com/example/payroll/LoadDatabase.java
@@ -12,19 +12,22 @@ class LoadDatabase {
   private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(EmployeeRepository repository) {
+  CommandLineRunner initDatabase(EmployeeRepository employeeRepository, OrderRepository orderRepository) {
 
     return args -> {
-      log.info("Preloading " + repository.save(new Employee("Bilbo", "Baggins", "burglar")));
-      log.info("Preloading " + repository.save(new Employee("Frodo", "Baggins", "thief")));
-    };
-  }
+      employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+      employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
 
-  @Bean
-  CommandLineRunner initDatabaseTest() {
-    log.info("log outside args arrow");
-    return args -> {
-      log.info("log inside args arrow");
+      employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+
+      
+      orderRepository.save(new Order("MacBook Pro", Status.COMPLETED));
+      orderRepository.save(new Order("iPhone", Status.IN_PROGRESS));
+
+      orderRepository.findAll().forEach(order -> {
+        log.info("Preloaded " + order);
+      });
+      
     };
   }
 }
---
+++ payroll/src/main/java/com/example/payroll/OrderRepository.java
@@ -0,0 +1,5 @@
+package com.example.payroll;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface OrderRepository extends JpaRepository<Order, Long> {
+}
